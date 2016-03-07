db015125ed5e563c2f82ac6a92cdb2cf7626b7dc
ppalumbo17 <ppalumbo@mines.edu>
2016-03-07 16:14:49 -0700

    Table Created for Project 4

--- project-4.sql
+++ project-4.sql
@@ -1,4 +1,21 @@
 /* Peter Palumbo*/
 /* Project 4*/
 
+/* Create the music database*/
+CREATE TABLE ppalumbo.music(
+	artist_type text,
+	artist_name text,
+	artist_id serial,
+	album_title text,
+	album_year numeric(4,0),
+	album_id serial,
+	album_genre text,
+	label_name text,
+	label_location text,
+	label_id serial,
+	song_name text,
+	song_number integer);
 
+/*Define Columns*/
+COMMENT ON COLUMN ppalumbo.music.artist_type IS 'Type of Artist';
+COMMENT ON COLUMN ppalumbo.music.artist_name IS 'Name of Artist';
